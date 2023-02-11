39d2383189ef364d7c5b17ed19073ea463563e7c
Rokas <rookas.rudzenskas@gmail.com>
2023-02-11 10:44:54 +0200

    feat: add chat row
    - Add chat row to the chat list
    - Add chat row to the chat list
    - Add chat row to the chat list

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Link from "next/link";
 import {ChatBubbleLeftIcon} from "@heroicons/react/24/outline";
 import {TrashIcon} from "@heroicons/react/24/outline";
+import {usePathname, useRouter} from "next/navigation";
+import {useSession} from "next-auth/react";
+import {useCollection} from "react-firebase-hooks/firestore";
+import {collection, orderBy, query} from "@firebase/firestore";
+import {db} from "../firebase";
 
 type Props = {
   id: string
 }
 const ChatRow = ({id}: Props) => {
+  const pathName = usePathname()
+  const router = useRouter()
+  const {data: session} = useSession()
+  const [active, setActive] = useState(false)
+  const [messages] = useCollection(query(
+    collection(db, 'users', session?.user?.email!, 'chats', id, 'messages'),
+    orderBy('createdAt', 'asc')
+  ))
+
   return (
     <Link className={`chatRow justify-center`} href={`chat/${id}`}>
       <ChatBubbleLeftIcon className="h-5 w-5" />
