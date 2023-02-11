90902603b1baf13f89a62f2fc66cac25d681fc7b
Rokas <rookas.rudzenskas@gmail.com>
2023-02-11 12:21:57 +0200

    Add ClientProvider
    This is a test commit message.

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default async function RootLayout({
           <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
             <Sidebar/>
           </div>
+          <ClientProvider />
           <div className="bg-[#343541] flex-1">{children}</div>
         </div>
       )}
---
+++ components/ClientProvider.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react';
+import {Toaster} from "react-hot-toast";
+
+const ClientProvider = ({}) => {
+  return (
+    <>
+     <Toaster position={'top-right'} />
+    </>
+  );
+};
+
+export default ClientProvider;
+// by Rokas with ❤️
