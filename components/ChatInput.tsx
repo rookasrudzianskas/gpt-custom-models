b3ccfcb65ca63a22e5fcc1f92c2547bba79eda7c
Rokas <rookas.rudzenskas@gmail.com>
2023-02-11 11:05:25 +0200

    feat: add chat input
    This commit adds a chat input to the chat window.
    
    The chat input is a form with a text input and a button.
    
    The text input is a controlled component that is bound to the prompt state.
    
    The button is a paper airplane icon.

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+'use client'
+import React, {useState} from 'react';
+import {PaperAirplaneIcon} from "@heroicons/react/20/solid";
 
 type Props = {
   chatId: string
 }
 
 const ChatInput = ({chatId}: Props) => {
+  const [prompt, setPrompt] = useState('')
   return (
     <div>
-      CjatInput
+      <form className="p-5 space-x-5 flex-1">
+        <input type="text"
+               placeholder="Type a message"
+               value={prompt}
+               onChange={(e) => setPrompt(e.target.value)}
+        />
+        <button>
+          <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+        </button>
+      </form>
+      <div>
+
+      </div>
     </div>
   );
 };
