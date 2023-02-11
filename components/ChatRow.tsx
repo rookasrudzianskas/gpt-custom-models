aee0c0f36cb0d9b6426a42f2047b553cd12f5275
Rokas <rookas.rudzenskas@gmail.com>
2023-02-11 10:56:24 +0200

    feat: add delete chat functionality
    - Add delete chat functionality
    - Add delete icon to chat row

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -5,7 +5,7 @@ import {TrashIcon} from "@heroicons/react/24/outline";
 import {usePathname, useRouter} from "next/navigation";
 import {useSession} from "next-auth/react";
 import {useCollection} from "react-firebase-hooks/firestore";
-import {collection, orderBy, query} from "@firebase/firestore";
+import {collection, deleteDoc, doc, orderBy, query} from "@firebase/firestore";
 import {db} from "../firebase";
 
 type Props = {
@@ -23,13 +23,18 @@ const ChatRow = ({id}: Props) => {
     setActive(pathName.includes(id))
   }, [pathName])
 
+  const removeChat = async () => {
+    await deleteDoc(doc(db, 'users', session?.user?.email!, 'chats', id))
+    router.replace('/')
+  }
+
   return (
     <Link className={`chatRow justify-center ${active && 'bg-gray-700/50'}`} href={`chat/${id}`}>
       <ChatBubbleLeftIcon className="h-5 w-5" />
       <p className="flex-1 hidden md:inline-flex truncate">
         {messages?.docs[messages?.docs.length - 1]?.data().text() || 'New Chat'}
       </p>
-      <TrashIcon className="h-5 w-5 text-gray-700 hover:text-red-700" />
+      <TrashIcon onClick={() => removeChat()} className="h-5 w-5 text-gray-700 hover:text-red-700" />
     </Link>
   );
 };
