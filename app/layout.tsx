04a55b20d4347c841628e0d6cc229944cb99467a
Rokas <rookas.rudzenskas@gmail.com>
2023-02-11 09:49:49 +0200

    feat: add next-auth to the project
    - Add next-auth to the project
    - Add a login page
    - Add a signup page
    - Add a logout page
    - Add a session provider
    - Add a session provider to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,25 +2,28 @@ import '../styles/globals.css'
 import React from "react";
 import Sidebar from '../components/Sidebar';
 import {SessionProvider} from "../components/SessionProvider";
+import { getServerSession } from 'next-auth'
+import {authOptions} from "../pages/api/auth/[...nextauth]";
 
-export default function RootLayout({
-  children,
-}: {
+export default async function RootLayout({
+                                           children,
+                                         }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession(authOptions)
   return (
     <html>
-      <head />
-      <body>
-      <SessionProvider session={pageProps.session}>
-        <div className="flex">
-          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
-            <Sidebar />
-          </div>
-          <div className="bg-[#343541] flex-1">{children}</div>
+    <head/>
+    <body>
+    <SessionProvider session={session}>
+      <div className="flex">
+        <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+          <Sidebar/>
         </div>
-      </SessionProvider>
-      </body>
+        <div className="bg-[#343541] flex-1">{children}</div>
+      </div>
+    </SessionProvider>
+    </body>
     </html>
   )
 }
