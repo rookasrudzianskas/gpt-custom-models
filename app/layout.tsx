0c836e729c051a7e2825fbbc51a81665bb7c047c
Rokas <rookas.rudzenskas@gmail.com>
2023-02-11 09:48:25 +0200

    Add SessionProvider to layout
    This is a breaking change.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import React from "react";
 import Sidebar from '../components/Sidebar';
+import {SessionProvider} from "../components/SessionProvider";
+
 export default function RootLayout({
   children,
 }: {
@@ -10,12 +12,14 @@ export default function RootLayout({
     <html>
       <head />
       <body>
-      <div className="flex">
-        <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
-          <Sidebar />
+      <SessionProvider session={pageProps.session}>
+        <div className="flex">
+          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+            <Sidebar />
+          </div>
+          <div className="bg-[#343541] flex-1">{children}</div>
         </div>
-        <div className="bg-[#343541] flex-1">{children}</div>
-      </div>
+      </SessionProvider>
       </body>
     </html>
   )
