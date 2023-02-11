97b0a63f44aa3947b6c44f26301597cd64e7dfad
Rokas <rookas.rudzenskas@gmail.com>
2023-02-11 09:56:29 +0200

    Add Google sign in button
    This commit adds a Google sign in button to the login page.
    
    The button is styled to match the rest of the site.

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,12 +1,15 @@
 'use client'
 import React from 'react';
 import Image from "next/image";
+import {signIn} from "next-auth/react";
 
 const Login = ({}) => {
   return (
-    <div>
+    <div className="bg-[#11A37F] flex flex-col h-screen items-center justify-center text-center">
       <Image src={'https://links.papareact.com/2i6'} width={300} height={300} alt={'logo'} />
-      <button>Sign in to use ChatGPT</button>
+      <button
+        onClick={() => signIn('google')}
+        className="text-white font-bold text-3xl animate-pulse">Sign in to use ChatGPT</button>
     </div>
   );
 };
