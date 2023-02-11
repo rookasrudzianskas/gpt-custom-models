454694846a586d6cd0417ab99569440781f6875a
Rokas <rookas.rudzenskas@gmail.com>
2023-02-11 10:03:48 +0200

    feat: add user image to sidebar
    - Add user image to sidebar
    - Add sign out button

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,10 @@
+'use client'
 import React from 'react';
 import NewChat from "./NewChat";
+import {signOut, useSession} from "next-auth/react";
 
 const Sidebar = ({}) => {
+  const { data: session } = useSession()
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -14,6 +17,13 @@ const Sidebar = ({}) => {
           {/*  Chat list*/}
         </div>
       </div>
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image || ''}
+          alt="image"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"/>
+      )}
     </div>
   );
 };
