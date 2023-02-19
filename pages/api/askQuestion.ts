790d72214c4604b921295388ead7a7d5880ef731
Rokas <rookas.rudzenskas@gmail.com>
2023-02-19 17:45:05 +0200

    fix

--- pages/api/askQuestion.ts
+++ pages/api/askQuestion.ts
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import query from "../../lib/queryApi";
+import admin from "firebase-admin";
+import {adminDb} from "../../firebaseAdmin";
 
 type Data = {
   answer: string
@@ -13,17 +16,29 @@ export default async function handler(
   const {prompt, chatId, model, session} = req.body
 
   if(!prompt) {
-    res.status(400).json({ answer: 'No prompt provided' })
+    res.status(400).json({ answer: 'Please provide a prompt!' })
     return
   }
 
   if(!chatId) {
-    res.status(400).json({ answer: 'No chatId provided' })
+    res.status(400).json({ answer: 'Please provide the valid chat ID!' })
     return
   }
 
   // Chat gpt query
-  const response = await query(prompt, chatId,model)
+  const response = await query(prompt, chatId, model)
+
+  const message: Message = {
+    text: response || 'ChatGPT was unable to find an answer to your question. Please try again later.',
+    createdAt: admin.firestore.Timestamp.now(),
+    user: {
+      _id: 'CHatGPT',
+      name: 'ChatGPT',
+      avatar: 'https://links.papareact.com/89k',
+    }
+  };
+
+  await adminDb.collection('users').doc(session?.user?.email).collection('chats').doc(chatId).collection('messages').add(message);
 
   res.status(200).json({ name: 'John Doe' })
 }
